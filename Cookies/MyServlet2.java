21d622747369da2d40ed52b8703c6aaed8516b6e
redduke-9 <82090730+redduke-9@users.noreply.github.com>
2021-04-21 23:49:06 +0530

    Add files via upload

---
+++ Cookies/MyServlet2.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.Cookie;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class MyServlet2
+ */
+@WebServlet("/MyServlet2")
+public class MyServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			response.setContentType("text/html");
+			PrintWriter p=response.getWriter();
+			Cookie a[]=request.getCookies();
+			p.print("name:    " +a[0].getValue());
+			p.print("password is:   " +a[1].getValue());
+			p.close();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
